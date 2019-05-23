b8c78aa08780bee23ea38165c0f630b5879119b8
scott austin <scott.austin@users.noreply.github.com>
2019-05-23 17:09:37 +0100

    target read & write

---
+++ src/adapter/pg/read/target.go
@@ -0,0 +1,50 @@
+package read
+
+import (
+	"database/sql"
+	"github.com/liampm/static-code-analysis-tool/domain"
+	"github.com/satori/go.uuid"
+)
+
+type PostgresTargetReadRepo struct {
+	db *sql.DB
+}
+
+func TargetRepo(db *sql.DB) *PostgresTargetReadRepo {
+	return &PostgresTargetReadRepo{db: db}
+}
+
+func (repo *PostgresTargetReadRepo) Find(id uuid.UUID) (domain.Target, error) {
+	target := domain.Target{}
+
+	row := repo.db.QueryRow("SELECT * FROM target WHERE id = $1", id)
+
+	// Populate the entity with the information from the row
+	err := row.Scan(&target.Id, &target.ProjectId &target.Name)
+
+	if err == sql.ErrNoRows {
+		return target, err // Return the error so that it can be dealt with
+	} else if err != nil {
+		panic(err) // Panic whilst we're in development
+	}
+
+	return target, nil
+}
+
+func (repo *PostgresTargetReadRepo) AllForProject(id uuid.UUID) []domain.Target {
+	rows, err := repo.db.Query("SELECT * FROM target WHERE projectId = $1", id)
+
+	if err != nil {
+		panic(err) // Panic whilst we're in development
+	}
+
+	targets := []domain.Target{}
+
+	for rows.Next() {
+		target := domain.Target{}
+		rows.Scan(&target.Id,  &target.ProjectId &target.Name)
+		targets = append(targets, target)
+	}
+
+	return projects
+}
---
+++ src/adapter/pg/write/target.go
@@ -0,0 +1,62 @@
+package write
+
+import (
+	"database/sql"
+	"github.com/liampm/static-code-analysis-tool/domain"
+	uuid "github.com/satori/go.uuid"
+)
+
+type PostgresTargetRepo struct {
+	db *sql.DB
+}
+
+func TargetRepo(db *sql.DB) *PostgresTargetRepo {
+	return &PostgresTargetRepo{db: db}
+}
+
+func (repo *PostgresTargetRepo) Save(target domain.Target) {
+	_, err := repo.find(target.Id)
+
+	if err == sql.ErrNoRows {
+		repo.insert(target)
+		return
+	} else if err == nil {
+		repo.update(target)
+		return
+	}
+
+	panic(err) // Panic whilst we're in development
+}
+
+func (repo *PostgresTargetRepo) find(id uuid.UUID) (target domain.Target, err error) {
+	target = domain.Target{}
+
+	row := repo.db.QueryRow("SELECT * FROM target WHERE id = $1", id)
+
+	// Populate the entity with the information from the row
+	err = row.Scan(&target.Id, &target.ProjectId &target.Name)
+
+	if err == sql.ErrNoRows {
+		return target, err // Return the error so that it can be dealt with
+	} else if err != nil {
+		panic(err) // Panic whilst we're in development
+	}
+
+	return target, nil
+}
+
+func (repo *PostgresTargetRepo) insert(target domain.Target) {
+	_, err := repo.db.Exec("INSERT INTO target (id, name) VALUES ($1, $2, $3)", target.Id, target.projectId, target.Name)
+
+	if err != nil {
+		panic(err) // Panic whilst we're in development
+	}
+}
+
+func (repo *PostgresTargetRepo) update(target domain.Target) {
+	_, err := repo.db.Exec("UPDATE target SET id = $1, projectId = $2 name = $3", target.Id, target.projectId, target.Name)
+
+	if err != nil {
+		panic(err) // Panic whilst we're in development
+	}
+}
